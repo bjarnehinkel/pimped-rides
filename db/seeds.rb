e7660697ad219ad87a35e9cc247925bdec0d843e
Majd Sufian <majdnewsufian@gmail.com>
2020-02-26 12:23:42 +0100

    Frontend of the show page done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+Listing.destroy_all
 
 # Listings.destroy_all
 
