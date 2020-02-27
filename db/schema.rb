fe62f48c142f457b3b1937fe0e8304e203c4cb61
Bjarne Hinkel <bjarne@hinkels.de>
2020-02-27 12:32:52 +0100

    added three columns to listings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_114406) do
+ActiveRecord::Schema.define(version: 2020_02_27_113127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,9 @@ ActiveRecord::Schema.define(version: 2020_02_25_114406) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.integer "price"
+    t.text "description"
+    t.integer "horsepower"
+    t.string "engine"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
