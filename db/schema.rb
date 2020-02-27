8ab3c1bd718fa7fe5b51cc72ff3ab1d64a1845e5
Bjarne Hinkel <bjarne@hinkels.de>
2020-02-27 12:08:44 +0100

    migrated with base migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_133025) do
+ActiveRecord::Schema.define(version: 2020_02_25_114406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,10 +52,7 @@ ActiveRecord::Schema.define(version: 2020_02_25_133025) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "start_date"
-    t.string "end_date"
-    t.integer "prices"
-    t.string "photo_code"
+    t.integer "price"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
